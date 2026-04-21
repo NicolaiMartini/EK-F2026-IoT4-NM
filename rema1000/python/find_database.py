f47255253158012d428882772e09d99945459a40
NicolaiMartini <152643549+NicolaiMartini@users.noreply.github.com>
2026-04-21 08:36:34 +0200

    coupling/cohesion WIP.

--- rema1000/python/find_database.py
+++ rema1000/python/find_database.py
@@ -6,28 +6,43 @@ Hvis dette gøres sucesfuldt 8 ud af 10 gange, anses kravet som opfyldt.
 """
 
 import re
-from zipfile import ZipFile as zf
 import mimetypes
+import tarfile as tf
+import zipfile as zf
 
-def find_database(database):
+def find_database_type(database:str):
     try:
-        kind = mimetypes.guess_file_type(database)
-        print(kind)
-        # with zf(database) as zip:
-        #     zip_content = zf.namelist(zip)
-        #     for i in zip_content:
-        #         matches = re.findall("*rema1000*", i, re.IGNORECASE)
-        #         print(matches)
+        if zf.is_zipfile(database):
+            return "zip"
+            print(zf.is_zipfile(database))
+            print(database, "is a zipfile.")
+            # with zf.ZipFile(database) as zip:
+                # zip_content = zf.ZipFile.namelist(zip)
+                # for item in zip_content:
+                #     re_matches = re.findall("*rema1000*", item, re.IGNORECASE)
+                #     print(re_matches)
+        elif tf.is_tarfile(database):
+            return "tar"
+            print(database, " is a tarball")
+        else:
+            return "unknown"
+            print(database, " is of type ", mimetypes.guess_file_type(database))
+
+
+        # kind = mimetypes.guess_file_type(database)
+        # # print(database, kind[0])
+        # if "zip" in kind[0]:
+        #     with zf(database) as zip:
+        #         zip_content = zf.namelist(zip)
+        #         for i in zip_content:
+        #             matches = re.findall("*rema1000*", i, re.IGNORECASE)
+        #             print(matches)
+        # elif "tar" in kind[0]:
+        #     with tf.open("")
     except Exception as e:
         print(e)
 
 
-print(f"AFU: {find_database("AFU_EXTRACTION_FFS.zip")}")
-find_database("EXTRACTION_BFU.zip")
-find_database("filesystem.tar")
-
-
-# with zf("AFU_EXTRACTION_FFS.zip") as z:
-#     AFU_content = zf.namelist(AFU)
-#     for i in AFU_content:
-#         print(i)
+find_database_type("AFU_EXTRACTION_FFS.zip")
+# find_database_type("EXTRACTION_BFU.zip")
+# find_database_type("filesystem.tar")
