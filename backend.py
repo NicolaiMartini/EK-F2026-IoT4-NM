7a7b69a51f1792dfd46241805a649ca32a818b3d
NicolaiMartini <152643549+NicolaiMartini@users.noreply.github.com>
2026-04-22 15:33:20 +0200

    Formatted output, automatic DST time conversion.

--- backend.py
+++ backend.py
@@ -23,7 +23,6 @@ def find_archive_type(database: str):
 def find_database_location(database, regexstring):
     """
     Find the in-archive location of desired databases.\n
-    Please input the archive to search through.\n
     Please input a regexstring to search inside the archive. The search is case insensitive.
     """
     try:
@@ -32,7 +31,7 @@ def find_database_location(database, regexstring):
             with zipfile.ZipFile(database) as zip:
                 zip_content = zipfile.ZipFile.namelist(zip)
                 for item in zip_content:
-                    match=re.search(regexstring, item, re.IGNORECASE)
+                    match = re.search(regexstring, item, re.IGNORECASE)
                     if match:
                         re_matches.append(match.group())
             return "zip", re_matches
@@ -48,13 +47,13 @@ def find_database_location(database, regexstring):
         print(e)
 
 
-def extract_known_databases(database:str,regex_string:str,output_directory:str):
+def extract_known_databases(database: str, regex_string: str, output_directory: str):
     """
     Extract the databases to the provided output_directory. Make sure the output directory already exists. \n
     Please input a regexstring. The search is case insensitive.
     """
     try:
-        databases=find_database_location(database,regex_string)
+        databases = find_database_location(database, regex_string)
         if databases[0] == "zip":
             with zipfile.ZipFile(database) as extract_zip:
                 for item in databases[1]:
@@ -63,7 +62,7 @@ def extract_known_databases(database:str,regex_string:str,output_directory:str):
                         # print(f"skipping '{item} - already exists")
                         continue
                     # print(f"Extracting: {item_path}")
-                    extract_zip.extract(member=item,path=output_directory)
+                    extract_zip.extract(member=item, path=output_directory)
 
         elif databases[0] == "tar":
             with tarfile.open(database) as extract_tar:
@@ -73,32 +72,70 @@ def extract_known_databases(database:str,regex_string:str,output_directory:str):
                         # print(f"skipping '{item} - already exists")
                         continue
                     # print(f"Extracting: {item_path}")
-                    extract_tar.extract(member=item,path=output_directory)
+                    extract_tar.extract(member=item, path=output_directory)
     except Exception as e:
         print(e)
 
 def list_dir_recursively(directory):
+    """
+    Retrieve the contents of a directory, recursively.
+    """
     try:
-        contents=[]
+        contents = []
         for root, dirs, files in os.walk(directory):
             for name in files:
-                contents.append(os.path.join(root,name))
+                contents.append(os.path.join(root, name))
         return contents
     except Exception as e:
         return e
 
+
 def get_db_tables(database):
+    """
+    Retrieve the tables of the specified database.
+    """
     try:
         with sqlite3.connect(database) as sql:
-            cur=sql.cursor()
+            cur = sql.cursor()
             cur.execute("""SELECT name 
                         FROM sqlite_master 
                         WHERE type='table' 
                         AND name NOT LIKE 'sqlite_%'
                         ORDER BY name
                         ;""")
-            tables = [row[0] for row in cur.fetchall()]
+            tables = [
+                row[0] for row in cur.fetchall()
+            ]  # fetch only first item in 1-item tuple, of each row
             return tables
+    except sqlite3.Error as e:
+        print(e)
+    except Exception as e:
+        print(e)
+
+
+def get_table_headers(database, table):
+    """
+    Retrieve the table headers of the specified table.
+    """
+    with sqlite3.connect(database) as sql:
+        cur = sql.cursor()
+        cur.execute(f"SELECT * FROM {table};")
+        column_names = [description[0] for description in cur.description]
+        return column_names
+
+
+def retrieve_db_table_content(database, table):
+    """
+    This will print all content from the specified table of the specified database.
+    """
+    try:
+        with sqlite3.connect(database) as sql:
+            cur = sql.cursor()
+            cur.execute(f"""
+                        SELECT *
+                        FROM {table};
+                        """)
+            return cur.fetchall()
     except sqlite3.Error as e:
         print(e)
     except Exception as e:
--- rema1000/extract_table_content.py
+++ rema1000/extract_table_content.py
@@ -1,27 +1,60 @@
-import os
 import re
 import sys
+import sqlite3
 import tempfile
 from pathlib import Path
-sys.path.insert(0, str(Path(__file__).resolve().parents[1])) # Add project root directory to path
+from zoneinfo import ZoneInfo
+from datetime import datetime,timezone
+
+sys.path.insert(
+    0, str(Path(__file__).resolve().parents[1])
+)  # Add project root directory to path
 import backend
 
-ALL_REMA1000_DB=r".*rema1000.*.\.db.*" # Find all (.db, -wal, -shm, -journal) related to Rema1000.
-REMA1000_RECEIPT_DB=r".*rema1000.*receipts\.db.*" # Find all .db, -wal, -shm and -journal for a specific db. This is crucial - WAL is standard in most android phones today, and excluding the .db-extensions might provide you with an empty db.
-AFU="databaser/AFU.zip"
+ALL_REMA1000_DB = (
+    r".*rema1000.*.\.db.*"  # Find all (.db, -wal, -shm, -journal) related to Rema1000.
+)
+REMA1000_RECEIPT_DB = r".*rema1000.*receipts\.db.*"  # Find all .db, -wal, -shm and -journal for a specific db. This is crucial - WAL is standard in most android phones today, and excluding the .db-extensions will probably provide an empty db.
+CLEAN_DB = r".*db$"  # Only match files ending in exactly .db
+AFU = "databaser/AFU.zip"
 
-if __name__=="__main__":
+if __name__ == "__main__":
     try:
-        with tempfile.TemporaryDirectory(delete=True) as tmpdir: #pass delete=False in TemporaryDirectory() to keep the files to inspect, in case output is unexpected
-            backend.extract_known_databases(AFU,REMA1000_RECEIPT_DB,tmpdir)
-            databases=backend.list_dir_recursively(tmpdir) # returns a list, but i just want the content, ergo indexing
-            pattern=re.compile(r".*db$")
-            matches=[]
+        with tempfile.TemporaryDirectory(
+            delete=0
+        ) as tmpdir:  # pass delete=False in TemporaryDirectory() to keep the files to inspect, in case output is unexpected
+            backend.extract_known_databases(AFU, REMA1000_RECEIPT_DB, tmpdir)
+            databases = backend.list_dir_recursively(
+                tmpdir
+            )  # returns a list, but i just want the content, ergo indexing
+            matches = []
             for database in databases:
-                match=pattern.search(database)
+                match = re.search(CLEAN_DB, database)
                 if match:
-                    matches.append(match.group()) # Creates a list with rel. databases in it
-            receipts=matches[0] # Only 1 item in the list.
-            print(backend.get_db_tables(receipts))
+                    matches.append(
+                        match.group()
+                    )  # Creates a list with rel. databases in it
+            receipts_db = matches[0]  # Only 1 item in the list.
+            receipt_table_name = backend.get_db_tables(
+                receipts_db
+            )[0] # I want to access the first table in the db
+            with sqlite3.connect(receipts_db) as sql:
+                cur=sql.cursor()
+                cur.execute(f"""
+                            SELECT searchText, paymentDate, paymentSource, pp_id, pp_cardType, pp_maskedPan
+                            from {receipt_table_name};
+                            """) #paymentDate is declared in ms, /1000 to be used in datetime.
+                for item in cur.fetchall():
+                    parts = item[0].split(';')[:3]
+                    modified_first_item = ';'.join(parts) + ';'
+                    modified_tuple = (modified_first_item,) + item[1:]
+                    print(f"""
+                          Location: {modified_tuple[0]}
+                          Date and time: {datetime.fromtimestamp(modified_tuple[1]/1000, tz=ZoneInfo("Europe/Copenhagen")).strftime('%Y-%m-%d %H:%M:%S')}
+                          Payment method: {modified_tuple[2]}
+                          Payment ID: {modified_tuple[3]}
+                          Card Type: {modified_tuple[4]}
+                          Masked PAN: {modified_tuple[5]}
+                          """)
     except Exception as e:
         print(e)
