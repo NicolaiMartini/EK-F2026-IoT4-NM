a44cb540aa357e16f83d9a1cf988134a2c1b0c3a
NicolaiMartini <152643549+NicolaiMartini@users.noreply.github.com>
2026-06-04 13:32:05 +0200

    Minor alt. to 07. ALEAPP combined and improved. Project as a smaller CLI script.

--- rema_1000.py
+++ rema_1000.py
@@ -1,4 +1,16 @@
 __artifacts_v2__ = {
+        "rema1000_receipt_raw": {
+        "name": "Rema1000 Receipts",
+        "description": "Extracts Rema1000 receipts from the android app 'Rema1000 | Scan Selv'. All raw data.",
+        "author": "Nicolai Martini",
+        "version": "1.0",
+        "date": "2026-06-04",
+        "requirements": "Cellebrite UFED After First Unlock data acquisition, or similar",
+        "category": "Rema1000 | Scan Selv",
+        "notes": "forensics data of supermarket habit and location insights.",
+        "paths": ("*/dk.rema1000.app/databases/receipts.db*",),
+        "function": "get_receipts_raw"
+    },
     "rema1000_receipt_prettified": {
         "name": "Rema1000 Receipts, prettified",
         "description": "Extracts Rema1000 receipts from the android app 'Rema1000 | Scan Selv'. All data is focused and prettified, and supportive data from */dk.rema1000.app/databases/shopdatabase.db is used for items column.",
@@ -6,22 +18,59 @@ __artifacts_v2__ = {
         "version": "1.0",
         "date": "2026-06-04",
         "requirements": "Cellebrite UFED After First Unlock data acquisition, or similar",
-        "category": "Supermarket",
+        "category": "Rema1000 | Scan Selv",
         "notes": "forensics data of supermarket habit and location insights.",
         "paths": ("*/dk.rema1000.app/databases/shopdatabase.db*","*/dk.rema1000.app/databases/receipts.db*"),
         "function": "get_receipts_prettified"
     }
 }
 
+
 from datetime import datetime
 from zoneinfo import ZoneInfo
-from scripts.ilapfuncs import artifact_processor, open_sqlite_db_readonly, get_file_path, logfunc
+from scripts.ilapfuncs import open_sqlite_db_readonly, logfunc
+from scripts.artifact_report import ArtifactHtmlReport
+
+def get_receipts_raw(files_found, report_folder, seeker, wrap_text):  
+    report = ArtifactHtmlReport("Rema 1000")
+    report_name = "Rema 1000, raw"
+    report.start_artifact_report(report_folder,report_name)
+    report.add_script()
+    for file_found in files_found:
+        file_found = str(file_found)
+        if file_found.endswith('receipts.db'):
+            data_headers = ('id','displayId','paymentDate','paymentSource',
+                            'storeNumber','totalPrice','totalPriceString',
+                            'totalDiscount','totalVat','Chargeback',
+                            'searchText','zipString','pp_id','pp_cardType','pp_maskedPan')
+            db = open_sqlite_db_readonly(file_found)
+            cur = db.cursor()
+            cur.execute(f"""
+                        SELECT *
+                        FROM ReceiptEntity;
+                        """)
+            rows=cur.fetchall()
+            entries=len(rows)
+            if entries>0:
+                entries_list=[]
+                for row in rows:
+                    list_row=list(row)
+                    for i in range(len(list_row)):
+                        if list_row[i] is None:
+                            list_row[i]="None"
+                    entries_list.append(list_row)
+                report.write_artifact_data_table(data_headers,entries_list,files_found[1])
+                report.end_artifact_report()
+            else:
+                logfunc('No Rema1000 | Scan & Go data available')
 
 
-@artifact_processor
 def get_receipts_prettified(files_found, report_folder, seeker, wrap_text):  
     available_products=[]
-    source_path = get_file_path(files_found, "receipts.db")  
+    report = ArtifactHtmlReport("Rema 1000")
+    report_name = "Rema 1000, formatted"
+    report.start_artifact_report(report_folder,report_name)
+    report.add_script()
     for file_found in files_found:
         if file_found.endswith('shopdatabase.db'):
             prod_db=open_sqlite_db_readonly(file_found)
@@ -63,6 +112,7 @@ def get_receipts_prettified(files_found, report_folder, seeker, wrap_text):
                     prettified_list[5]=list_row[5]
                     prettified_list[6]=list_row[6]
                     entries_list.append(prettified_list)
-                return data_headers, entries_list, source_path
+                report.write_artifact_data_table(data_headers,entries_list,files_found[5])
+                report.end_artifact_report()
             else:
                 logfunc('No Rema1000 | Scan & Go data available')
